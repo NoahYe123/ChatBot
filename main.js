2499b5f7fd7877bdd05ae8ce31746d4f28ef5cfc
NoahYe123 <jian.ye@mail.mcgill.ca>
2022-01-22 00:05:22 -0500

    Noahh commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 //adding comement
+//NOah
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
