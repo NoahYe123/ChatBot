84285cff8332bff72312758472b7df57a0950110
hiArturo <arturo.moryramirez@mail.mcgill.ca>
2022-01-22 00:19:46 -0500

    comment22

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 //adding comement
-//NOah
+//danny comments
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
