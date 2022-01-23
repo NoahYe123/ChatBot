7800d6fa55a9dd4d0be16216fc635a4b340e67e4
NoahYe123 <jian.ye@mail.mcgill.ca>
2022-01-23 00:05:41 -0500

    final

--- index.js
+++ index.js
@@ -2,76 +2,81 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
-
-
- 
-//adding comement
-//danny comments
- 
-const currentDate = new Date();
 client.on("ready", () => {
 
 
- console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`)
 });
 
+
 const cron = require('node-cron');
-var morining = cron.schedule(" 0 8 * * *", ()=>{
+var morining = cron.schedule(" 0 8 * * *", () => {
   channel = client.channels.cache.get("934293745187315715");
   channel.send("Good Morning! Hope You Have A Great Day");
-},{ 
+}, {
   scheduled: true,
   timezone: "EST"
-  });
+});
 
-  const cron = require('node-cron');
-var morining = cron.schedule(" 0 21 * * *", ()=>{
+var morining = cron.schedule(" 0 21 * * *", () => {
   channel = client.channels.cache.get("934293745187315715");
   channel.send("Go to sleep! It's getting late");
-},{ 
+}, {
   scheduled: true,
   timezone: "EST"
-  });
+});
 
 // for spamming function
-var task = cron.schedule("* * * * * *", ()=>{
+var task = cron.schedule("* * * * * *", () => {
   channel = client.channels.cache.get("934293745187315715");
-  channel.send("Have a good meal");
-},{ 
+  channel.send("spam");
+}, {
   scheduled: false,
   timezone: "EST"
-  });
+});
 
 
 client.on("message", msg => {
 
-  if(msg.content == "spam"){
-  task.start();
+
+  if (msg.content.startsWith('!react')) {
+
+    msg.channel.send('How You Feeling?').then((question) => {
+      question.react('😀');
+      question.react('😎');
+      question.react('😭');
+      question.react('😴');
+      question.react('😡');
+      question.react('😈');
+    });
   }
 
+  if (msg.content == "spam") {
+    task.start();
+  }
 
-  if(msg.content == "stop"){
+  if (msg.content == "stop") {
     task.stop();
-    }
+  }
 
- if (msg.content === "ping") {
-   msg.reply("pong");
- }
- if (msg.content === "hi") {
-   msg.reply("How are you");
- }
- 
- if (msg.content === "Date") {
-   msg.reply(currentDate.toLocaleString());
- }
- 
- if (msg.content === "p") {
- const botInfo = new Discord.MessageEmbed()
-     .setDescription('[Click here!](https://support.discord.com/hc/en-us/community/posts/360038398572-Hyperlink-Markdown)')
-    
-     msg.channel.send(botInfo.description);
- }
- 
- })
- 
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+  if (msg.content === "hi") {
+    msg.reply("How are you");
+  }
+
+  if (msg.content === "Date") {
+    msg.reply(currentDate.toLocaleString());
+  }
+
+  if (msg.content === "p") {
+    const botInfo = new Discord.MessageEmbed()
+      .setDescription('[Click here!](https://support.discord.com/hc/en-us/community/posts/360038398572-Hyperlink-Markdown)')
+
+    msg.channel.send(botInfo.description);
+  }
+
+})
+
 client.login("OTM0MjkyODI0MzMyMDc1MDg4.Yet9_w.-tIhTkimADEwQLS6JIbLKyELnak");
