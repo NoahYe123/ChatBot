86f6ec5ea5f6b01e68c6b41300be73692908f5f9
Capt0bvious <abiola.olaniyan15@gmail.com>
2022-01-23 02:45:19 -0500

    Merged with Noah's features

--- index.js
+++ index.js
@@ -3,15 +3,48 @@ const { getVoiceConnection, joinVoiceChannel, createAudioPlayer, createAudioReso
 const { addSpeechEvent } = require('discord-speech-recognition') // import speech recognition dependency
 const discordTTS = require("discord-tts") // discord text to speech
 
+const currentDate = new Date()
+
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] }) // create a new discord server/bot client
 //adding comement
 //danny comments
 
 // Print message when bot is ready
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
 
+
+  console.log(`Logged in as ${client.user.tag}!`)
+});
+
+// gives a sweer good morening text @ 8am
+const cron = require('node-cron');
+var morining = cron.schedule(" 0 8 * * *", () => {
+  channel = client.channels.cache.get("934293745187315715");
+  channel.send("Good Morning! Hope You Have A Great Day");
+}, {
+  scheduled: true,
+  timezone: "EST"
+});
+// Gives you a sweet goodnight text @ 8 am
+var morining = cron.schedule(" 00 21 * * *", () => {
+  channel = client.channels.cache.get("934293745187315715");
+  channel.send("Go to sleep! It's Getting Late!");
+}, {
+  scheduled: true,
+  timezone: "EST"
+});
+
+// for spamming function
+var task = cron.schedule("* * * * * *", () => {
+  channel = client.channels.cache.get("934293745187315715");
+  channel.send("spam");
+}, {
+  scheduled: false,
+  timezone: "EST"
+});
+
+
+  
 const player = createAudioPlayer(); // create audio player for new options
 addSpeechEvent(client); // set up speech recognition
 
@@ -37,8 +70,31 @@ var chanId = undefined
 
 // Message commands
 client.on("messageCreate", async msg => {
+
+  if (msg.content.startsWith('!react')) {
+
+    msg.channel.send('How You Feeling?').then((question) => {
+      question.react('😀');
+      question.react('😎');
+      question.react('😭');
+      question.react('😴');
+      question.react('😡');
+      question.react('😈');
+    });
+  }
+// start spam
+  if (msg.content == "!spam") {
+    task.start();
+  }
+// stop the spam
+  if (msg.content == "!stop") {
+    task.stop();
+  }
+
+
   // Summon allows bot to join voice channel and play a greeting
   if (msg.content === "!summon") {
+    console.log("passed here")
     voiceId = msg.guild.id // assigns voice channel ids for later use
     chanId = msg.member.voice.channel.id
     // join voice channel
@@ -48,10 +104,23 @@ client.on("messageCreate", async msg => {
       adapterCreator: msg.guild.voiceAdapterCreator,
       selfDeaf: false
     }).subscribe(player)
+    console.log("passed here123")
 
     let newGreeting = createAudioResource(__dirname + '\\audio_files' + '\\ohayo_noahahahaha.mp3');
     player.play(newGreeting)
+
+    console.log("passed here")
   }
+
+  if (msg.content === "!hi") {
+    msg.reply("How are you?");
+  }
+// Tells current date
+  if (msg.content === "!date") {
+    msg.reply(currentDate.toLocaleString());
+  }
+
+
 })
 
 // Speech commands
